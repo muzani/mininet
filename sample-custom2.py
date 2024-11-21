7d8e2b588fef3376c9eba89ec9ac1075d85e30d8
muzani <zanimumu@gmail.com>
2024-11-21 08:03:41 +0700

    customtopo

--- sample-custom2.py
+++ sample-custom2.py
@@ -19,15 +19,17 @@ class MyTopo( Topo ):
         # Add hosts and switches
         h1 = self.addHost( 'h1' )
         h2 = self.addHost( 'h2' )
-        s3 = self.addSwitch( 's3' )
-        s4 = self.addSwitch( 's4' )
-        s5 = self.addSwitch( 's5' )
+        h3 = self.addHost( 'h3' )
+        s3 = self.addSwitch( 's1' )
+        s4 = self.addSwitch( 's2' )
+        s5 = self.addSwitch( 's3' )
 
         # Add links
-        self.addLink( s3, s4 )
-        self.addLink( s3, s5 )
-        self.addLink( s4, h1 )
-        self.addLink( s5, h2 )
+        self.addLink( s1, s2 )
+        self.addLink( s2, s3 )
+        self.addLink( s1, h1 )
+        self.addLink( s2, h2 )
+        self.addLink( s3, h3 )
 
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
