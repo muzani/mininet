5f2bc90df86326b58f388560eda73041773d9a65
muzani <zanimumu@gmail.com>
2024-11-21 08:05:24 +0700

    custom baru

--- sample-custom2.py
+++ sample-custom2.py
@@ -20,9 +20,9 @@ class MyTopo( Topo ):
         h1 = self.addHost( 'h1' )
         h2 = self.addHost( 'h2' )
         h3 = self.addHost( 'h3' )
-        s3 = self.addSwitch( 's1' )
-        s4 = self.addSwitch( 's2' )
-        s5 = self.addSwitch( 's3' )
+        s1 = self.addSwitch( 's1' )
+        s2 = self.addSwitch( 's2' )
+        s3 = self.addSwitch( 's3' )
 
         # Add links
         self.addLink( s1, s2 )
