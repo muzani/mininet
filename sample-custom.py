2cafebfc2c5e267c6fd3ede9436d7a3e85b56e67
muzani <zanimumu@gmail.com>
2024-11-21 08:08:45 +0700

    sample custom topo

---
+++ sample-custom.py
@@ -0,0 +1,33 @@
+"""Custom topology example
+
+Two directly connected switches plus a host for each switch:
+
+   host --- switch --- switch --- host
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def build( self ):
+        "Create custom topo."
+
+        # Add hosts and switches
+        h1 = self.addHost( 'h1' )
+        h2 = self.addHost( 'h2' )
+        s3 = self.addSwitch( 's3' )
+        s4 = self.addSwitch( 's4' )
+        s5 = self.addSwitch( 's5' )
+
+        # Add links
+        self.addLink( s3, s4 )
+        self.addLink( s3, s5 )
+        self.addLink( s4, h1 )
+        self.addLink( s5, h2 )
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
