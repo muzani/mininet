7f6c9b29954b66c2cedacabc1751311cf8c86bef
muzani <zanimumu@gmail.com>
2024-11-28 04:01:38 +0700

    add ip

--- sample-custom2.py
+++ sample-custom2.py
@@ -25,7 +25,7 @@ class MyTopo( Topo ):
         # h2 = self.addHost( 'h2' )
         h1 = self.addHost( 'h1' ,'ip=192.168.1.2')
         # h3 = self.addHost( 'h3' )
-        h1 = self.addHost( 'h1' ,'ip=192.168.1.1')
+        h1 = self.addHost( 'h1' ,'ip=192.168.1.3')
         
         #add Switchs
         s1 = self.addSwitch( 's1' )
