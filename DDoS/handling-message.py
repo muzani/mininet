8f1360e1134dd488621686343bb017f24e79454f
muzani <zanimumu@gmail.com>
2024-12-24 13:52:26 +0700

    asasad

--- DDoS/handling-message.py
+++ DDoS/handling-message.py
@@ -4,32 +4,37 @@ from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet, ipv4
-import switch
 
-class SimpleSwitch(switch.SimpleSwitch13):
+
+class SimpleSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch, self).__init__(*args, **kwargs)
         self.logger.info("start init")
 
-    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
+    #@set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):        
         msg = ev.msg
         dp = msg.datapath
-        ofproto = dp.ofproto
-        parser = dp.ofproto_parser
-        self.logger.info("paket handler")
-
+        ofp = dp.ofproto
+        ofp_parser = dp.ofproto_parser
+        
         # Proses paket masuk
         in_port = msg.match['in_port']
         self.logger.info("Packet received at switch %s from port %s",
                          dp.id, in_port)
 
-        actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
-        out = parser.OFPPacketOut(
-            datapath=dp, buffer_id=msg.buffer_id, in_port=in_port, actions=actions
-        )
+        actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
+
+        data = None
+        if msg.buffer_id == ofp.OFP_NO_BUFFER:
+             data = msg.data
+
+        out = ofp_parser.OFPPacketOut(
+            datapath=dp, buffer_id=msg.buffer_id, in_port=msg.in_port,
+            actions=actions, data = data)
         dp.send_msg(out)
 
 
