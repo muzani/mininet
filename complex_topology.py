df6fd30be0092e53ed411f4b95fdf2fbd8be63ba
muzani <zanimumu@gmail.com>
2024-12-24 14:24:39 +0700

    asda

--- complex_topology.py
+++ complex_topology.py
@@ -19,31 +19,30 @@ class MyTopo( Topo ):
 
     def build( self ):
         "Create custom topo."
-        net = Mininet(controller=RemoteController, switch=OVSSwitch, link=TCLink)
-
+        
         # Tambahkan switch
-        s1 = net.addSwitch('s1')
-        s2 = net.addSwitch('s2')
-        s3 = net.addSwitch('s3')
-        s4 = net.addSwitch('s4')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
 
         # Tambahkan host
-        h1 = net.addHost('h1', ip='10.0.0.1/24')
-        h2 = net.addHost('h2', ip='10.0.0.2/24')
-        h3 = net.addHost('h3', ip='10.0.0.3/24')
-        h4 = net.addHost('h4', ip='10.0.0.4/24')
+        h1 = self.addHost('h1', ip='10.0.0.1/24')
+        h2 = self.addHost('h2', ip='10.0.0.2/24')
+        h3 = self.addHost('h3', ip='10.0.0.3/24')
+        h4 = self.addHost('h4', ip='10.0.0.4/24')
 
         # Hubungkan host ke switch
-        net.addLink(h1, s1, bw=10, delay='5ms')
-        net.addLink(h2, s1, bw=10, delay='5ms')
-        net.addLink(h3, s4, bw=10, delay='5ms')
-        net.addLink(h4, s4, bw=10, delay='5ms')
+        self.addLink(h1, s1, bw=10, delay='5ms')
+        self.addLink(h2, s1, bw=10, delay='5ms')
+        self.addLink(h3, s4, bw=10, delay='5ms')
+        self.addLink(h4, s4, bw=10, delay='5ms')
 
         # Hubungkan switch ke switch
-        net.addLink(s1, s2, bw=15, delay='2ms')
-        net.addLink(s2, s3, bw=15, delay='2ms')
-        net.addLink(s3, s4, bw=15, delay='2ms')
-        net.addLink(s1, s4, bw=20, delay='1ms') 
+        self.addLink(s1, s2, bw=15, delay='2ms')
+        self.addLink(s2, s3, bw=15, delay='2ms')
+        self.addLink(s3, s4, bw=15, delay='2ms')
+        self.addLink(s1, s4, bw=20, delay='1ms') 
         
 topos = { 'mytopo': ( lambda: MyTopo() ) }
 
