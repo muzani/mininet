c1588f2d90f141a239bacb2c0696190c7be7ab07
muzani <zanimumu@gmail.com>
2024-12-24 18:47:00 +0700

    asa

--- complex_topology.py
+++ complex_topology.py
@@ -24,7 +24,7 @@ class MyTopo( Topo ):
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
         s3 = self.addSwitch('s3')
-        s4 = self.addSwitch('s4')
+        #s4 = self.addSwitch('s4')
 
         # Tambahkan host
         h1 = self.addHost('h1', ip='10.0.0.1/24')
@@ -34,15 +34,15 @@ class MyTopo( Topo ):
 
         # Hubungkan host ke switch
         self.addLink(h1, s1, bw=10, delay='5ms')
-        self.addLink(h2, s1, bw=10, delay='5ms')
-        self.addLink(h3, s4, bw=10, delay='5ms')
-        self.addLink(h4, s4, bw=10, delay='5ms')
+        self.addLink(h2, s2, bw=10, delay='5ms')
+        self.addLink(h3, s3, bw=10, delay='5ms')
+        self.addLink(h4, s1, bw=10, delay='5ms')
 
         # Hubungkan switch ke switch
         self.addLink(s1, s2, bw=15, delay='2ms')
         self.addLink(s2, s3, bw=15, delay='2ms')
-        self.addLink(s3, s4, bw=15, delay='2ms')
-        self.addLink(s1, s4, bw=20, delay='1ms') 
+        #self.addLink(s3, s4, bw=15, delay='2ms')
+        self.addLink(s1, s3, bw=20, delay='1ms') 
         
 topos = { 'mytopo': ( lambda: MyTopo() ) }
 
