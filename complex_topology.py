56bf45c75d55ffc6bcf8b4bce8ace9b17668c00a
muzani <zanimumu@gmail.com>
2024-12-24 19:01:49 +0700

    asa

--- complex_topology.py
+++ complex_topology.py
@@ -18,31 +18,51 @@ class MyTopo( Topo ):
     "Simple topology example."
 
     def build( self ):
-        "Create custom topo."
-        
+        """Membuat topologi jaringan yang kompleks dengan koneksi yang valid."""
+        net = Mininet(controller=RemoteController, switch=OVSSwitch, link=TCLink)
+
+        # Tambahkan controller
+        controller = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
+
         # Tambahkan switch
-        s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
-        s3 = self.addSwitch('s3')
-        #s4 = self.addSwitch('s4')
+        s1 = net.addSwitch('s1')
+        s2 = net.addSwitch('s2')
+        s3 = net.addSwitch('s3')
+        s4 = net.addSwitch('s4')
 
         # Tambahkan host
-        h1 = self.addHost('h1', ip='10.0.0.1/24')
-        h2 = self.addHost('h2', ip='10.0.0.2/24')
-        h3 = self.addHost('h3', ip='10.0.0.3/24')
-        h4 = self.addHost('h4', ip='10.0.0.4/24')
+        h1 = net.addHost('h1', ip='10.0.0.1/24', defaultRoute='via 10.0.0.254')
+        h2 = net.addHost('h2', ip='10.0.0.2/24', defaultRoute='via 10.0.0.254')
+        h3 = net.addHost('h3', ip='10.0.0.3/24', defaultRoute='via 10.0.0.254')
+        h4 = net.addHost('h4', ip='10.0.0.4/24', defaultRoute='via 10.0.0.254')
 
         # Hubungkan host ke switch
-        self.addLink(h1, s1, bw=10, delay='5ms')
-        self.addLink(h2, s2, bw=10, delay='5ms')
-        self.addLink(h3, s3, bw=10, delay='5ms')
-        self.addLink(h4, s1, bw=10, delay='5ms')
+        net.addLink(h1, s1, bw=10, delay='5ms')
+        net.addLink(h2, s2, bw=10, delay='5ms')
+        net.addLink(h3, s3, bw=10, delay='5ms')
+        net.addLink(h4, s4, bw=10, delay='5ms')
 
         # Hubungkan switch ke switch
-        self.addLink(s1, s2, bw=15, delay='2ms')
-        self.addLink(s2, s3, bw=15, delay='2ms')
-        #self.addLink(s3, s4, bw=15, delay='2ms')
-        self.addLink(s1, s3, bw=20, delay='1ms') 
+        net.addLink(s1, s2, bw=15, delay='2ms')
+        net.addLink(s2, s3, bw=15, delay='2ms')
+        net.addLink(s3, s4, bw=15, delay='2ms')
+        net.addLink(s4, s1, bw=15, delay='2ms')  # Jalur alternatif untuk konektivitas penuh
+
+        # Konfigurasi jaringan
+        net.start()
+
+        # Pastikan semua switch terhubung ke controller
+        for switch in [s1, s2, s3, s4]:
+            switch.cmd('ovs-vsctl set-controller %s tcp:127.0.0.1:6633' % switch.name)
+
+        # Uji koneksi antar host
+        net.pingAll()
+
+        # Jalankan CLI untuk pengujian manual
+        CLI(net)
+
+        # Hentikan jaringan
+        net.stop()
         
 topos = { 'mytopo': ( lambda: MyTopo() ) }
 
