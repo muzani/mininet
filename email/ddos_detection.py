fb9bf37a0a3ea87855b102c38448c8ffb467b439
muzani <zanimumu@gmail.com>
2025-01-01 20:54:00 +0700

    dasda

--- email/ddos_detection.py
+++ email/ddos_detection.py
@@ -56,7 +56,7 @@ class EmailNotificationRyu(app_manager.RyuApp):
         ip_pkt = pkt.get_protocol(ipv4.ipv4)
         icmp_pkt = pkt.get_protocol(icmp.icmp)
         
-        if ip_pkt or icmp_pkt:
+        if ip_pkt and icmp_pkt:
             src_ip = ip_pkt.src
             dest_ip = ip_pkt.dst
             self.packet_counts[src_ip] += 1
