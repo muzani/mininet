2ffa57f3fe07fc557a9d17169abde83f8ba7b164
muzani <zanimumu@gmail.com>
2025-01-01 21:36:19 +0700

    asda

--- email/ddos_detection.py
+++ email/ddos_detection.py
@@ -38,6 +38,8 @@ class EmailNotificationRyu(app_manager.RyuApp):
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
+        """Menambahkan flow saat switch pertama kali terhubung ke controller."""
+        
         # Kirim email notifikasi saat switch baru terhubung
         # switch_id = ev.msg.datapath.id
         # subject = "Notifikasi SDN - Switch Baru Terhubung"
@@ -50,6 +52,7 @@ class EmailNotificationRyu(app_manager.RyuApp):
     
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
+        """Menangani paket yang datang ke controller."""
         msg = ev.msg
         pkt = packet.Packet(msg.data)
         
@@ -58,6 +61,7 @@ class EmailNotificationRyu(app_manager.RyuApp):
             ip_pkt = pkt.get_protocol(ipv4.ipv4)
             icmp_pkt = pkt.get_protocol(icmp.icmp)
             
+            # Log untuk paket yang diterima
             if ip_pkt and icmp_pkt:
                 src_ip = ip_pkt.src
                 dest_ip = ip_pkt.dst
@@ -71,14 +75,26 @@ class EmailNotificationRyu(app_manager.RyuApp):
     def install_default_flow(self, datapath):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        match = parser.OFPMatch(eth_type=0x0800, ip_proto=1)  # ICMP (ETH_TYPE=0x0800, IP_PROTO=1)()
+        
+        # Membuat match untuk menangani semua paket (default flow)
+        match = parser.OFPMatch(eth_type=0x0800)  # ICMP (ETH_TYPE=0x0800, IP_PROTO=1)()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        
+        # Menambahkan flow ke switch untuk menangani semua paket
         self.add_flow(datapath, 0, match, actions)
 
     def add_flow(self, datapath, priority, match, actions):
+        """Menambahkan flow ke switch."""
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
         mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
         datapath.send_msg(mod)
         
+
+#penjelasan program
+# Event Handler  switch_features_handler : adalah event handler yang akan dipanggil ketika switch pertama kali terhubung ke controller. Di sini, kita akan menambahkan flow untuk memastikan bahwa paket yang datang dikirimkan ke controller untuk diproses lebih lanjut
+# Flow Default match = parser.OFPMatch() digunakan untuk mencocokkan semua paket. kita bisa menambahkan kriteria untuk memfilter jenis paket tertentu (misalnya, berdasarkan IP, protokol, atau alamat MAC)
+# Flow Default actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]: Ini mengatur bahwa setiap paket yang datang akan dikirim ke controller (menggunakan OFPP_CONTROLLER), dan tidak menggunakan buffer
+# Menambahkan Flow ke Switch add_flow() menambahkan flow dengan prioritas tertentu ke dalam tabel aliran switch
+# Packet In Handler adalah event handler yang akan dipanggil setiap kali paket masuk ke controller. kita dapat menambahkan logika untuk memproses paket tersebut di dalam fungsi ini
