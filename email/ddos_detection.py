2c87dce100a0b484cf14a77329cb7baefbc4ae71
muzani <zanimumu@gmail.com>
2025-01-01 21:54:21 +0700

    asd

--- email/ddos_detection.py
+++ email/ddos_detection.py
@@ -4,8 +4,6 @@ from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, set_ev_cl
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet, ethernet, ipv4, icmp
 from collections import defaultdict
-from ryu.ofproto import ofproto_v1_3
-from ryu.ofproto.ofproto_v1_3 import OFPActionOutput
 
 # Fungsi pengiriman email
 import smtplib
