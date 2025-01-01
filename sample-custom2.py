13c69cb7d5a1e0efeca467a1d92cdcb879317f4e
muzani <zanimumu@gmail.com>
2025-01-01 19:49:37 +0700

    ssdfsd

--- sample-custom2.py
+++ sample-custom2.py
@@ -28,6 +28,9 @@ class MyTopo( Topo ):
         h1 = self.addHost( 'h1' ,ip='192.168.1.1')
         h2 = self.addHost( 'h2' ,ip='192.168.1.2')
         h3 = self.addHost( 'h3' ,ip='192.168.1.3')
+        h4 = self.addHost( 'h4' ,ip='192.168.1.4')
+        h5 = self.addHost( 'h5' ,ip='192.168.1.5')
+        h6 = self.addHost( 'h6' ,ip='192.168.1.6')
         
         #add Switchs
         s1 = self.addSwitch( 's1') #, cls=OVSBridge
@@ -36,6 +39,9 @@ class MyTopo( Topo ):
 
         # Add links between host and switchs
         self.addLink( h1, s1 )
+        self.addLink( h4, s1 )
+        self.addLink( h5, s1 )
+        self.addLink( h6, s1 )
         self.addLink( h2, s2 )
         self.addLink( h3, s3 )
         
