d6aa4a470d25a4b89f0ae9a15dcae769b34789b2
muzani <zanimumu@gmail.com>
2025-01-02 12:19:05 +0700

    as

--- DDoS/ddos_detection_2.py
+++ DDoS/ddos_detection_2.py
@@ -161,7 +161,7 @@ class DDoSDetection(app_manager.RyuApp):
             print("len(self.mac_ip_to_dp[src] = ",len(self.mac_ip_to_dp[src]))
             
             #if(len(self.mac_ip_to_dp[src]) > 50):
-            if self.packet_counts[src_ip] > 30 and icmp_pkt:
+            if self.packet_counts[srcip] > 30 and icmp_pkt:
                 self.ddos_oocurs=True
                 print("DDos occur from src ", src)
                 match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
